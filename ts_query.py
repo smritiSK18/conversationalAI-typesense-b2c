05b1bf88c66c3ea8f70890a956138b5290429e9d
smritiSK18 <smriti.kashyap@pharmeasy.in>
2025-02-26 12:45:36 +0530

    Update ts_query.py

--- ts_query.py
+++ ts_query.py
@@ -1,9 +1,6 @@
 #NOT USING
 
-# 1-4 query - works
 from typesense_client2 import client
-#from document import response
-#from ss_documents import *
 import typesense
 
 # Function to search all documents
@@ -20,7 +17,6 @@ def search_all():
         print("Error in search_all:", e)
 
 
-#print('/////////')
 # Function to filter documents
 def search_with_filter():
     try:
@@ -35,13 +31,13 @@ def search_with_filter():
     except Exception as e:
         print("Error in search_with_filter:", e)
 
-# Function to perform fuzzy search
+# Function to perform fuzzy search (not an exact match) - through 'num typos' field
 def fuzzy_search():
     try:
         results = client.collections['products'].documents.search({
             'q': 'Product',
             'query_by': 'name',
-            'num_typos': 0  #didn't find where it is stored/specified?, also works for all
+            'num_typos': 3 
         })
         print("*** Fuzzy Search Results:")
         for hit in results['hits']:
@@ -50,7 +46,7 @@ def fuzzy_search():
         print("Error in fuzzy_search:", e)
 
 # Add more query functions as needed
-def child_query(): 
+def test_query(): 
     try:
         # Search for all documents
         results = client.collections['products'].documents.search({
